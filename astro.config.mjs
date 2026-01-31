da7c868f7c3ef737f444b0898c77328cf77b0fd4
dyingbreed243 <dyingbreed243@gmail.com>
2026-01-31 05:36:44 +0000

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [preact()],
+  integrations: [preact(), tailwind(), icon({
+    include: {
+      mdi: ["*"],
+    }
+  })],
   adapter: node({
     mode: "standalone"
   })
